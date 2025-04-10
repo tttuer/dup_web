630a1a11ffdf89cf3b86b648cf0f75305724f1de
tttuer <tttuer1@naver.com>
2025-04-10 10:13:03 +0900

    prettier

--- src/components/lists/Searchbar.vue
+++ src/components/lists/Searchbar.vue
@@ -4,20 +4,27 @@ import Dropdown from "./Dropdown.vue";
 
 const emit = defineEmits(["search"]);
 const search = ref("");
-const searchOptions = ['설명+첨부파일', '금액'];
+const searchOptions = ["설명+첨부파일", "금액"];
 const searchToEnum = {
-  '설명+첨부파일': 'DESCRIPTION_FILENAME',
-  '금액': 'PRICE',
-}
+  "설명+첨부파일": "DESCRIPTION_FILENAME",
+  금액: "PRICE",
+};
 
 watch(search, () => {
-  emit(["search", "searchOption"], { 'search': search, 'searchOption': searchToEnum[searchOptions] });
+  emit(["search", "searchOption"], {
+    search: search,
+    searchOption: searchToEnum[searchOptions],
+  });
 });
 </script>
 
 <template>
   <div class="flex">
-    <Dropdown class="mr-2" :options="searchOptions" :nameToEnum="searchToEnum" />
+    <Dropdown
+      class="mr-2"
+      :options="searchOptions"
+      :nameToEnum="searchToEnum"
+    />
     <input
       class="h-9 rounded-sm border border-gray-300 pl-2"
       type="text"
