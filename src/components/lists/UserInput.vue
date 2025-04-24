d4cbc86788bcd7dfe0a15c7a4a9f1424d3a66f34
tttuer <tttuer1@naver.com>
2025-04-24 10:25:12 +0900

    저장 시 lock 버튼 추가

--- src/components/lists/UserInput.vue
+++ src/components/lists/UserInput.vue
@@ -30,23 +30,59 @@
               pattern="\d*"
             />
           </td>
-          <td class="w-90 px-4 py-2 text-left">
-            <div class="grid grid-cols-4 gap-4">
+          <td class="w-69 px-4 py-2 text-left">
+            <div class="flex gap-1.5">
               <input
+                :class="role === 'ADMIN' ? 'col-span-3' : 'col-span-4'"
                 ref="fileInput"
-                class="col-span-3 flex w-full rounded-sm border border-gray-300 pl-1"
+                class="flex w-full rounded-sm border border-gray-300 pl-1"
                 type="file"
                 multiple
                 accept=".pdf, .jpg, .jpeg, .png"
                 @change="handleFileChange"
               />
-              <button
-                class="col-span-1 w-15 cursor-pointer rounded-md border border-gray-300 p-0 px-1 py-0.5 text-sm hover:bg-black hover:text-white"
-                @click="saveFile"
+              <input id="lock" v-show="false" type="checkbox" v-model="isLocked" />
+              <label
+                v-show="role === 'ADMIN'"
+                for="lock"
+                class="flex h-6 w-6 cursor-pointer items-center justify-center rounded-md border border-gray-300 hover:bg-gray-200"
               >
-                입력
-              </button>
+                <svg
+                  v-if="isLocked"
+                  xmlns="http://www.w3.org/2000/svg"
+                  class="h-4 w-4 text-gray-800"
+                  fill="currentColor"
+                  viewBox="0 0 24 24"
+                >
+                  <path
+                    d="M12 17a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3zm6-6h-1V9a5 5 0 0 0-10 0v2H6a2 2 0 0 0-2 2v7a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2v-7a2 2 0 0 0-2-2zm-3 0H9V9a3 3 0 1 1 6 0v2z"
+                  />
+                </svg>
+                <svg
+                  v-else
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke-width="1.5"
+                  stroke="currentColor"
+                  class="size-3"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M13.5 10.5V6.75a4.5 4.5 0 1 1 9 0v3.75M3.75 21.75h10.5a2.25 2.25 0 0 0 2.25-2.25v-6.75a2.25 2.25 0 0 0-2.25-2.25H3.75a2.25 2.25 0 0 0-2.25 2.25v6.75a2.25 2.25 0 0 0 2.25 2.25Z"
+                  />
+                </svg>
+              </label>
             </div>
+          </td>
+          <td class="w-21">
+            <button
+              class="col-span-1 w-15 cursor-pointer rounded-md border border-gray-300 p-0 px-1 py-0.5 text-sm hover:bg-black hover:text-white"
+              @click="saveFile"
+            >
+              입력
+            </button>
           </td>
         </tr>
       </thead>
@@ -61,6 +97,9 @@ import { authFetch } from '../../utils/authFetch';
 import { useToast } from 'vue-toastification';
 import { useCurrencyFormatter } from '@/utils/currencyFormatter';
 import { useTypeStore } from '@/stores/typeStore';
+import { getRoleFromLocalStorage } from '@/utils/token';
+
+const role = ref(getRoleFromLocalStorage());
 
 const typeStore = useTypeStore();
 
@@ -79,6 +118,7 @@ const fileInput = ref(null);
 const emit = defineEmits(['createFiles']);
 const toast = useToast();
 const { formatted, price, formatCurrency } = useCurrencyFormatter();
+const isLocked = ref(false);
 
 function handleFileChange(e) {
   files.value = e.target.files;
@@ -91,6 +131,7 @@ async function saveFile(e) {
   formData.append('withdrawn_at', date.value);
   formData.append('name', description.value);
   formData.append('price', price.value);
+  formData.append('lock', isLocked.value);
 
   for (const file of files.value) {
     formData.append('file_datas', file);
---
+++ src/utils/token.js
@@ -0,0 +1,16 @@
+export function getRoleFromToken(token) {
+    try {
+      const payload = JSON.parse(atob(token.split('.')[1]));
+      return payload.role;
+    } catch (e) {
+      console.error('토큰 디코딩 실패:', e);
+      return null;
+    }
+  }
+  
+  export function getRoleFromLocalStorage() {
+    const token = localStorage.getItem('access_token');
+    if (!token) return null;
+    return getRoleFromToken(token);
+  }
+  
