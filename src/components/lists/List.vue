8a25463c5fb0f4f5408b69f30aa864d55b719c5a
tttuer <tttuer1@naver.com>
2025-04-25 10:14:25 +0900

    lock filter 추가

--- src/components/lists/List.vue
+++ src/components/lists/List.vue
@@ -6,6 +6,9 @@ import Sentinel from './Sentinel.vue';
 import DateSearch from './DateSearch.vue';
 import EditModal from './EditModal.vue';
 import { useTypeStore } from '@/stores/typeStore';
+import { getRoleFromLocalStorage } from '@/utils/token';
+
+const role = ref(getRoleFromLocalStorage());
 
 const typeStore = useTypeStore();
 
@@ -88,6 +91,7 @@ const fileUrl = `${import.meta.env.VITE_FILE_API_URL}`;
 const toast = useToast();
 const isEditModalOpen = ref(false);
 const editTargetFile = ref(null);
+const lockFilter = ref(false);
 
 function openEditModal(file) {
   editTargetFile.value = file;
@@ -118,6 +122,7 @@ async function fetchFiles(isReset = false) {
   params.append('page', currentPage.value);
   params.append('search', searchbar.value);
   params.append('search_option', searchbarOption.value);
+  params.append('is_locked', lockFilter.value);
 
   isLoading.value = true;
   isPdfConverting.value = true;
@@ -268,7 +273,7 @@ onUnmounted(() => {
   }
 });
 
-watch([selectedCompany, selectedDate], async () => {
+watch([selectedCompany, selectedDate, lockFilter], async () => {
   await fetchFiles(true);
 });
 </script>
@@ -349,7 +354,47 @@ watch([selectedCompany, selectedDate], async () => {
               <th class="w-45 px-4 py-2 text-left">날짜</th>
               <th class="min-w-48 truncate px-4 py-2 text-left">설명</th>
               <th class="w-45 px-4 py-2 text-left">금액</th>
-              <th class="w-90 px-4 py-2 text-left">첨부파일</th>
+              <th class="w-69 px-4 py-2 text-left">
+                <div class="flex justify-between">
+                  첨부파일
+                  <div>
+                    <input id="lock-filter" v-show="false" type="checkbox" v-model="lockFilter" />
+                    <label
+                      v-show="role === 'ADMIN'"
+                      for="lock-filter"
+                      class="flex h-6 w-6 cursor-pointer items-center justify-center rounded-md border border-gray-300 hover:bg-gray-200"
+                    >
+                      <svg
+                        v-if="lockFilter"
+                        xmlns="http://www.w3.org/2000/svg"
+                        class="h-4 w-4 text-gray-800"
+                        fill="currentColor"
+                        viewBox="0 0 24 24"
+                      >
+                        <path
+                          d="M12 17a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3zm6-6h-1V9a5 5 0 0 0-10 0v2H6a2 2 0 0 0-2 2v7a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2v-7a2 2 0 0 0-2-2zm-3 0H9V9a3 3 0 1 1 6 0v2z"
+                        />
+                      </svg>
+                      <svg
+                        v-else
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke-width="1.5"
+                        stroke="currentColor"
+                        class="size-3"
+                      >
+                        <path
+                          stroke-linecap="round"
+                          stroke-linejoin="round"
+                          d="M13.5 10.5V6.75a4.5 4.5 0 1 1 9 0v3.75M3.75 21.75h10.5a2.25 2.25 0 0 0 2.25-2.25v-6.75a2.25 2.25 0 0 0-2.25-2.25H3.75a2.25 2.25 0 0 0-2.25 2.25v6.75a2.25 2.25 0 0 0 2.25 2.25Z"
+                        />
+                      </svg>
+                    </label>
+                  </div>
+                </div>
+              </th>
+              <th class="w-21 px-4 py-2 text-left"></th>
             </tr>
           </thead>
         </table>
